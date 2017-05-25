db78466ac8942b718610e830acacdfe26948c543
Pekka <nospam@astudios.org>
2017-05-25 20:54:24 +0300

    Autocompleting works. Something's fucky with the resetting of active classes in table list, but oh well...

--- src/js/TableAutoCompleter.js
+++ src/js/TableAutoCompleter.js
@@ -8,37 +8,55 @@ const TableAutoCompleter = (prompt, tableNames, links) => {
   }
 
   const listenToEnter = () => {
-    document.addEventListener('keyup', e => {
-      if (e.keyCode === 13 && offerActive) {
-        console.log(offerActive + '!!')
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === 9 && offerActive) { // "Tab"
+        e.preventDefault()
+        injectOffer(offerActive)
       }
     })
   }
 
+  const injectOffer = (tableName) => {
+    const query = prompt.value
+    prompt.value = query.replace(getLastWord(query), tableName) + ' '
+  }
+
+  const getLastWord = (str) => {
+    const words = str.split(' ')
+    return words[words.length - 1]
+  }
+
   const listenToPrompt = () => {
     prompt.addEventListener('keyup', e => {
-      const words = prompt.value.split(' ')
-      const lastWord = words[words.length - 1]
+      const lastWord = getLastWord(prompt.value)
+
       if (lastWord.length < 2) {
         offerActive = false
         return
       }
+
       const matches = tableNames.filter(tableName => {
         if (tableName.startsWith(lastWord)) {
           return true
         }
       })
+
       if (matches.length < 1) {
         offerActive = false
         console.log('No matches')
         return
+      } else {
+        offerActive = matches[0]
       }
-      const tableNameOnOffer = matches[0]
-      offerActive = tableNameOnOffer
-      links.forEach(link => {
-        const method = link.getAttribute('href').split('#')[1] === tableNameOnOffer ? 'add' : 'remove'
-        link.classList[method]('autocomplete-candidate')
-      })
+
+      renderTableButtons()
+    })
+  }
+
+  const renderTableButtons = () => {
+    links.forEach(link => {
+      const method = link.getAttribute('href').split('#')[1] === offerActive ? 'add' : 'remove'
+      link.classList[method]('autocomplete-candidate')
     })
   }
 
